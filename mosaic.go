e65e02338cb43f96057f4be12708fc07bf2ef408
George Armhold <armhold@gmail.com>
2015-04-22 16:16:54 -0400

    * basic webapp

---
+++ mosaic.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"net/http"
+	"html/template"
+	"os"
+	"fmt"
+)
+
+var templates = template.Must(template.ParseFiles("static/welcome.html", "static/upload.html"))
+
+type Page struct {
+	Title string
+	Body  []byte
+}
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	p := &Page{Title: "Welcome"}
+	renderTemplate(w, "welcome.html", p)
+}
+
+func main() {
+	http.HandleFunc("/", handler)
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080"
+	}
+	fmt.Printf("using port: %s\n", port)
+	http.ListenAndServe(":" + port, nil)
+}
+
+func renderTemplate(w http.ResponseWriter, templatePath string, p *Page) {
+	err := templates.ExecuteTemplate(w, templatePath, p)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
+}
