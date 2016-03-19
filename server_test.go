a0b095d97122b5c42ccf93f8e7f9f0992672fbc5
George Armhold <armhold@gmail.com>
2016-03-19 13:46:23 -0400

    update tests for previous refactoring

--- server_test.go
+++ server_test.go
@@ -4,26 +4,29 @@ import (
 	"net/http"
 	"net/http/httptest"
 	"testing"
+	"os"
 )
 
 func TestHomeHandler(t *testing.T) {
 	req, _ := http.NewRequest("GET", "", nil)
 	w := httptest.NewRecorder()
-	homeHandler(w, req)
+	homeHandler().ServeHTTP(w, req)
 	if w.Code != http.StatusOK {
 		t.Errorf("Home page didn't return %v", http.StatusOK)
 	}
 }
 
-func TestInvalid_upload_id(t *testing.T) {
+func TestInvalidUploadId(t *testing.T) {
 	req, _ := http.NewRequest("GET", "/results/invalid_upload_id", nil)
 	w := httptest.NewRecorder()
 
 	expected := http.StatusBadRequest
 
-	resultsHandler(w, req)
+	uploadRootDir := os.TempDir()
 
-	if w.Code !=  expected {
+	resultsHandler(uploadRootDir).ServeHTTP(w, req)
+
+	if w.Code != expected {
 		t.Errorf("results page expected %v, got %v", expected, w.Code)
 	}
 }
